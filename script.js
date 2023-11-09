4ff6df42cf588d1ae7ef8daa4a1d2f356c5fc98d
sathya-teja <134315505+sathya-teja@users.noreply.github.com>
2023-11-09 23:55:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+/*toggleicon*/
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll=() => {
+    SpeechRecognitionResult.forEach(sec => {
+        let top=window.scrollY;
+        let offset=sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top<= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+        };
+    });
+
+    let header = document.querySelector('header')
+};
